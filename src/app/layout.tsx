5cc40eac895c55e24d5fa9255c8968cc99a8a245
Yasaman Mohammadi <118657398+yasibena@users.noreply.github.com>
2024-12-16 09:49:10 +0330

    Lesson 1 (#3)
    * add gitignore
    
    * change read me
    
    * init
    
    * remove unnecessary files and code
    Add vazirmatn font
    Add `hello,friend!` example
    
    * add dynamic route example

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from 'next/font/google'
+import "./globals.css";
+
+const vazirmatn = Vazirmatn({
+  subsets: ['latin','arabic'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "کتاب های من",
+  description: "خرید کتاب های مورد علاقه ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
