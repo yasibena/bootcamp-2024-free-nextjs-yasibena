7c206b39d6882155e0b1429f3623ec8c35b8c343
yasibena <118657398+yasibena@users.noreply.github.com>
2024-12-24 20:53:10 +0330

    add layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>
-        <header>This is a header. Added by reza</header>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
