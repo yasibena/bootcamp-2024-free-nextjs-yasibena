42535791e5dae8ef9926fa6180a2a1e140d66bd2
yasibena <118657398+yasibena@users.noreply.github.com>
2024-12-24 20:16:06 +0330

    remove unncessary code and files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>{children}</body>
+      <body>
+        <header>This is a header. Added by reza</header>
+        {children}
+      </body>
     </html>
   );
 }
