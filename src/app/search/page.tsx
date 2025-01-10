f0227c0daa091efea2270c0a22ecc19cb84041d9
yasibena <118657398+yasibena@users.noreply.github.com>
2025-01-10 16:47:56 +0330

    add filters context

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import styles from "./page.module.css";
-
-import CardComponent from "@/components/card/card.component";
-import FilterButtonComponent from "@/components/filter-button/filter-button-component";
 import FilterComponent from "./components/filter/filter.component";
+
+import FiltersProvider from "./providers/filters/filters.components";
+
+import styles from "./page.module.css";
 
 const items = Array(100)
   .fill(null)
@@ -12,30 +12,31 @@ const items = Array(100)
 
 export default function page() {
   return (
-    <div className={styles.page}>
-      <div className={styles.filters}>
-        <FilterComponent
-          title="زوج یا فرد"
-          options={[
-            { value: "even", label: "زوج" },
-            { value: "odd", label: "فرد" },
-          ]}
-        />
-        <FilterComponent
-          title="بخش پذیری"
-          options={[
-            { value: "two", label: "بخش پدیری بر 2" },
-            { value: "three", label: "بخش پدیری بر 3" },
-            { value: "five", label: "بخش پدیری بر 5" },
-            { value: "seven", label: "بخش پدیری بر 7" },
-          ]}
-        />
+    <FiltersProvider>
+      <div className={styles.page}>
+        <div className={styles.filters}>
+          <FilterComponent
+            title="زوج یا فرد"
+            options={[
+              { value: "even", label: "زوج" },
+              { value: "odd", label: "فرد" },
+            ]}
+          />
+          <FilterComponent
+            title="بخش پذیری"
+            options={[
+              { value: "three", label: "بخش پدیری بر 3" },
+              { value: "five", label: "بخش پدیری بر 5" },
+              { value: "seven", label: "بخش پدیری بر 7" },
+            ]}
+          />
+        </div>
+        <div className={styles.results}>
+          {items.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </div>
       </div>
-      <div className={styles.results}>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </div>
-    </div>
+    </FiltersProvider>
   );
 }
---
+++ src/app/search/providers/filters/filters.components.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { createContext, PropsWithChildren, useState } from "react";
+import { FiltersType } from "../../types/filters.type";
+
+type ContextValue = {
+  filters: FiltersType;
+  changeFilter: <TKey extends keyof FiltersType>(
+    key: TKey,
+    value: FiltersType[TKey]
+  ) => void;
+  removeFilter: <TKey extends keyof FiltersType>(key: TKey) => void;
+  clearAll: () => void;
+};
+
+export const FilterContext = createContext<ContextValue>({
+  filters: {},
+  changeFilter: () => {},
+  removeFilter: () => {},
+  clearAll: () => {},
+});
+
+type Props = PropsWithChildren;
+
+export default function FiltersProvider({ children }: Props) {
+  const [filters, setFilters] = useState<FiltersType>({});
+
+  const changeFilter = <TKey extends keyof FiltersType>(
+    key: TKey,
+    value: FiltersType[TKey]
+  ): void => {
+    setFilters((old) => ({ ...old, [key]: value }));
+  };
+
+  const removeFilter = <TKey extends keyof FiltersType>(key: TKey): void => {
+    setFilters((old) => {
+      const clone = { ...old };
+      delete clone[key];
+      return clone;
+    });
+  };
+
+  const clearAll = (): void => {
+    setFilters({});
+  };
+
+  return (
+    <FilterContext.Provider
+      value={{ filters, changeFilter, removeFilter, clearAll }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+}
